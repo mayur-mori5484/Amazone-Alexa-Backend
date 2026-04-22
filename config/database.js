1947388493b27cf8f471b60978548e928b0a46ef
mayur-mori5484 <morimayur2002@gmail.com>
2026-04-22 15:45:29 +0530

    remove dv connection file

--- config/database.js
+++ config/database.js
@@ -20,7 +20,7 @@ const sequelize = new Sequelize(
 
 (async () => {
     try {
-        await sequelize.authenticate();
+        // await sequelize.authenticate();
         console.log('✅ Connection has been established successfully.');
     } catch (error) {
         console.error('❌ Unable to connect to the database:', error);
